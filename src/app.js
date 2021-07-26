ed279d6ac2540bfec2336c58405dc675e18da129
Hao Dai <hao.dai@sap.com>
2021-07-26 15:14:22 +0800

    module5

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const {accounts, users, writeJSON} = require('./data');
 
 const app = new express();
 
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
+const { accounts, users, writeJSON } = require('./data.js');
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
@@ -20,42 +22,10 @@ app.get('/', async (req, res, next ) => {
     res.render('index', importData);
 });
 
-app.get('/savings', (req, res, next) => {
-    res.render('account', { account: accounts.savings});
-});
-
-app.get('/checking', (req, res, next) => {
-    res.render('account', { account: accounts.checking});
-});
-
-app.get('/credit', (req, res, next) => {
-    res.render('account', { account: accounts.credit});
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.get('/payment', (req, res) =>  res.render('payment', {account: accounts.credit}) );
-
-app.post('/transfer', (req, res) => {
-    
-    accounts[req.body.from].balance -= parseInt(req.body.amount);
-    accounts[req.body.to].balance += parseInt(req.body.amount);
-    writeJSON(accounts, 'accounts.json');
-    res.render('transfer', {message: "Transfer Completed"});
-});
-
-app.post('/payment', (req, res) => {
-    
-    accounts['credit'].balance -= parseInt(req.body.amount);
-    accounts['credit'].available += parseInt(req.body.amount);
-    writeJSON(accounts, 'accounts.json');
-    res.render('payment', { message: "Payment Successful", account: accounts.credit });
-
-})
 
 app.listen(3000, () => {
     console.log('PS Project Running on Port 3000');
