505c0ca7803eaaad71afc97588cd5cce9c9ec05d
Hao Dai <hao.dai@sap.com>
2021-07-27 10:39:25 +0800

    for checking

--- src/app.js
+++ src/app.js
@@ -1,33 +1,25 @@
 const fs = require('fs');
 const path = require('path');
-const express = require('express');
 
+const express = require('express');
 const app = new express();
+
+const { accounts, users, writeJSON } = require('./data.js');
 
 const accountRoutes = require('./routes/accounts.js')
 const servicesRoutes = require('./routes/services.js')
-const { accounts, users, writeJSON } = require('./data.js');
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
 
-app.get('/', async (req, res, next ) => {
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-    const importData = {
-        title: 'Account Summary',
-        accounts: accounts
-    }
-    res.render('index', importData);
-});
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
 app.use('/account', accountRoutes);
 app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-app.listen(3000, () => {
-    console.log('PS Project Running on Port 3000');
-});
-
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
--- src/data.js
+++ src/data.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-const accountData = fs.readFileSync(__dirname + '\\json\\accounts.json', {encoding:'utf-8'});
-const accounts  = JSON.parse(accountData);
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
 
-const userData = fs.readFileSync(__dirname + '\\json\\users.json', {encoding:'utf-8'});
-const users   = JSON.parse(userData);
-
-const writeJSON = (data, fileName) => {
-    let dataJSON = JSON.stringify(data, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', fileName), dataJSON, 'utf8');
+const writeJSON = () => {
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
 }
 
 module.exports = {
-    accounts, users, writeJSON
-}
+  accounts,
+  users,
+  writeJSON
+};
